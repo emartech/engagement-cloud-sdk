9d34ec35ab5d3ff91f4eef70b31153bcdd98b984
David Schuppa <32750715+davidSchuppa@users.noreply.github.com>
2024-01-17 09:51:21 +0100

    feat(push): broadcast pushToken from onNewToken SUITEDEV-34899
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: Andras Sarro <lordandras@users.noreply.github.com>
    Co-authored-by: matusekma <36794575+matusekma@users.noreply.github.com>

--- android-emarsys-sdk-fcm/src/main/java/com/emarsys/service/EmarsysFirebaseMessagingService.kt
+++ android-emarsys-sdk-fcm/src/main/java/com/emarsys/service/EmarsysFirebaseMessagingService.kt
@@ -1,13 +1,23 @@
 package com.emarsys.service
 
+import android.content.Intent
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 
 class EmarsysFirebaseMessagingService : FirebaseMessagingService() {
+    private companion object {
+        const val PUSH_TOKEN_INTENT_FILTER_ACTION = "com.emarsys.sdk.PUSH_TOKEN"
+        const val PUSH_TOKEN_INTENT_KEY = "pushToken"
+    }
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-
+        val intent = Intent().apply {
+            action = PUSH_TOKEN_INTENT_FILTER_ACTION
+            putExtra(PUSH_TOKEN_INTENT_KEY, token)
+        }
+        sendBroadcast(intent)
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
--- android-emarsys-sdk-hms/src/main/java/com/emarsys/service/EmarsysHuaweiMessagingService.kt
+++ android-emarsys-sdk-hms/src/main/java/com/emarsys/service/EmarsysHuaweiMessagingService.kt
@@ -1,12 +1,21 @@
 package com.emarsys.service
 
+import android.content.Intent
 import com.huawei.hms.push.HmsMessageService
 import com.huawei.hms.push.RemoteMessage
 
 class EmarsysHuaweiMessagingService : HmsMessageService() {
+    private companion object {
+        const val PUSH_TOKEN_INTENT_FILTER_ACTION = "com.emarsys.sdk.PUSH_TOKEN"
+        const val PUSH_TOKEN_INTENT_KEY = "pushToken"
+    }
     override fun onNewToken(token: String) {
         super.onNewToken(token)
-
+        val intent = Intent().apply {
+            action = PUSH_TOKEN_INTENT_FILTER_ACTION
+            putExtra(PUSH_TOKEN_INTENT_KEY, token)
+        }
+        sendBroadcast(intent)
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
