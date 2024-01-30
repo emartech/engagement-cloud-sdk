e0a902491d74931243566968df533cdb02b7fba4
LasOri <laszlo.oeri@gmail.com>
2024-01-30 14:31:51 +0100

    test(device-info-collector): modify optional type SUITEDEV-35039
    Co-authored-by: Andras Sarro <lordandras@users.noreply.github.com>
    Co-authored-by: davidSchuppa <davidSchuppa@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: matusekma <matusekma@users.noreply.github.com>

--- emarsys-sdk/src/androidInstrumentedTest/kotlin/com/emarsys/core/device/DeviceInfoCollectorTests.android.kt
+++ emarsys-sdk/src/androidInstrumentedTest/kotlin/com/emarsys/core/device/DeviceInfoCollectorTests.android.kt
@@ -27,7 +27,7 @@ class DeviceInfoCollectorTests {
 
     private lateinit var mockLanguageProvider: LanguageProvider
     private lateinit var mockUuidProvider: Provider<String>
-    private lateinit var mockStorage: TypedStorageApi<String>
+    private lateinit var mockStorage: TypedStorageApi<String?>
     private lateinit var timeZone: TimeZone
     private lateinit var mockAndroidPlatformInfoCollector: AndroidPlatformInfoCollector
 
