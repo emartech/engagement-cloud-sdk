5f415c3d2eaf3ce62a72bdb3eabe086c384d4502
davidSchuppa <davidSchuppa@users.noreply.github.com>
2024-05-14 14:40:58 +0200

    test(push): added missing test to PushTokenBroadcastReceiver SUITEDEV-35728
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: Andras Sarro <lordandras@users.noreply.github.com>
    Co-authored-by: matusekma <36794575+matusekma@users.noreply.github.com>

---
+++ emarsys-sdk/src/androidInstrumentedTest/kotlin/com/emarsys/mobileengage/push/PushTokenBroadcastReceiverTest.kt
@@ -0,0 +1,58 @@
+package com.emarsys.mobileengage.push
+
+import android.content.Intent
+import androidx.test.platform.app.InstrumentationRegistry
+import com.emarsys.api.push.PushInternalApi
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class PushTokenBroadcastReceiverTest {
+    private companion object {
+        const val PUSH_TOKEN = "testToken"
+    }
+
+    private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    private lateinit var mockPushApi: PushInternalApi
+    private lateinit var sdkDispatcher: CoroutineDispatcher
+    private lateinit var broadcastReceiver: PushTokenBroadcastReceiver
+
+    @Before
+    fun setUp() {
+        mockPushApi = mockk(relaxed = true)
+        sdkDispatcher = StandardTestDispatcher()
+        Dispatchers.setMain(sdkDispatcher)
+
+        broadcastReceiver = PushTokenBroadcastReceiver(sdkDispatcher, mockPushApi)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun testOnReceive_shouldCallPushApiWithToken() = runTest {
+        val intent = Intent().apply {
+            action = "com.emarsys.sdk.PUSH_TOKEN"
+            putExtra("pushToken", PUSH_TOKEN)
+        }
+
+        broadcastReceiver.onReceive(context, intent)
+
+        advanceUntilIdle()
+
+        coVerify { mockPushApi.registerPushToken(PUSH_TOKEN) }
+    }
+}
