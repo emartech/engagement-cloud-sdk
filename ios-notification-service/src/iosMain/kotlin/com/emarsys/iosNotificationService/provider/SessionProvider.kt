4300a9d93b4ecd0df7b8a5548cb7f1b89fb7288d
LasOri <LasOri@users.noreply.github.com>
2024-07-15 13:22:03 +0200

    feat(session): Add SessionProvider to NotificationService SUITEDEV-35915
    Co-authored-by: matusekma <matusekma@users.noreply.github.com>

---
+++ ios-notification-service/src/iosMain/kotlin/com/emarsys/iosNotificationService/provider/SessionProvider.kt
@@ -0,0 +1,16 @@
+package com.emarsys.iosNotificationService.provider
+
+import platform.Foundation.NSURLSession
+import platform.Foundation.NSURLSessionConfiguration
+
+class SessionProvider: Provider<NSURLSession> {
+
+    private val session: NSURLSession by lazy {
+        NSURLSession.sessionWithConfiguration(NSURLSessionConfiguration.defaultSessionConfiguration())
+    }
+
+    override fun provide(): NSURLSession {
+        return session
+    }
+
+}
