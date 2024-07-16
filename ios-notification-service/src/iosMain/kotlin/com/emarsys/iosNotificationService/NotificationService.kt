8855253674c511ef8271a6057856b34a75aba778
matusekma <36794575+matusekma@users.noreply.github.com>
2024-07-16 16:31:07 +0200

    refactor(notification-service): restructure async calls in didReceiveNotificationRequest SUITEDEV-35915
    Co-authored-by: Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

--- ios-notification-service/src/iosMain/kotlin/com/emarsys/iosNotificationService/NotificationService.kt
+++ ios-notification-service/src/iosMain/kotlin/com/emarsys/iosNotificationService/NotificationService.kt
@@ -10,7 +10,6 @@ import com.emarsys.iosNotificationService.provider.SessionProvider
 import com.emarsys.iosNotificationService.provider.UUIDProvider
 import kotlinx.cinterop.BetaInteropApi
 import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
@@ -38,7 +37,8 @@ import platform.UserNotifications.UNNotificationRequest
 import platform.UserNotifications.UNNotificationServiceExtension
 
 @BetaInteropApi
-class NotificationService(private val notificationCenter: NotificationCenterApi = NotificationCenter()) : UNNotificationServiceExtension() {
+class NotificationService(private val notificationCenter: NotificationCenterApi = NotificationCenter()) :
+    UNNotificationServiceExtension() {
 
     private val mutex = Mutex()
 
@@ -54,19 +54,19 @@ class NotificationService(private val notificationCenter: NotificationCenterApi 
         request: UNNotificationRequest,
         withContentHandler: (UNNotificationContent?) -> Unit
     ) {
-        contentHandler = withContentHandler
-        bestAttemptContent = request.content.mutableCopy() as UNMutableNotificationContent
-
         runBlocking {
-            val userInfo = bestAttemptContent.userInfo as Map<String, Any>
+            withContext(Dispatchers.Default) {
+                contentHandler = withContentHandler
+                bestAttemptContent = request.content.mutableCopy() as UNMutableNotificationContent
 
-            val actions = createActions(userInfo)
-            val attachments = createAttachments(userInfo)
-            val inApp = createInApp(userInfo)
+                val userInfo = bestAttemptContent.userInfo as Map<String, Any>
+                val actions = async { createActions(userInfo) }
+                val attachments = async { createAttachments(userInfo) }
+                val inApp = async { createInApp(userInfo) }
 
-            awaitAll(actions, attachments, inApp)
+                awaitAll(actions, attachments, inApp)
+            }
         }
-
         contentHandler(bestAttemptContent)
     }
 
@@ -75,71 +75,69 @@ class NotificationService(private val notificationCenter: NotificationCenterApi 
     }
 
     @OptIn(ExperimentalForeignApi::class)
-    private suspend fun createActions(userInfo: Map<String, Any>): Deferred<Unit> = withContext(Dispatchers.Default) {
-        async {
-            val ems = userInfo["ems"] as? Map<String, Any> ?: return@async
-            val actions = ems["actions"] as? List<Map<String, Any>> ?: return@async
-            val data = NSJSONSerialization.dataWithJSONObject(actions, NSJSONWritingPrettyPrinted, null)
-            val actionsJson = NSString.create(data!!, NSUTF8StringEncoding)!!.toString()
-            val actionModels: List<ActionModel> = Json.decodeFromString(actionsJson)
+    private suspend fun createActions(userInfo: Map<String, Any>) {
+        val ems = userInfo["ems"] as? Map<String, Any> ?: return
+        val actions = ems["actions"] as? List<Map<String, Any>> ?: return
+        val data = NSJSONSerialization.dataWithJSONObject(
+            actions,
+            NSJSONWritingPrettyPrinted,
+            null
+        )
+        val actionsJson = NSString.create(data!!, NSUTF8StringEncoding)!!.toString()
+        val actionModels: List<ActionModel> = Json.decodeFromString(actionsJson)
 
-            val notificationActions = actionModels.map {
-                val options =
-                    if (it is DismissActionModel) UNNotificationActionOptionDestructive else UNNotificationActionOptionForeground
-                UNNotificationAction.actionWithIdentifier(it.id, it.title, options)
-            }
-            val category = UNNotificationCategory.categoryWithIdentifier(
-                uuidProvider.provide().UUIDString(),
-                actions,
-                notificationActions,
-                UNNotificationCategoryOptionNone
-            )
-
-            notificationCenter.addCategory(category)
-
-            mutex.withLock {
-                bestAttemptContent.setCategoryIdentifier(category.identifier)
-            }
+        val notificationActions = actionModels.map {
+            val options =
+                if (it is DismissActionModel) UNNotificationActionOptionDestructive else UNNotificationActionOptionForeground
+            UNNotificationAction.actionWithIdentifier(it.id, it.title, options)
         }
+        val category = UNNotificationCategory.categoryWithIdentifier(
+            uuidProvider.provide().UUIDString(),
+            actions,
+            notificationActions,
+            UNNotificationCategoryOptionNone
+        )
+
+        notificationCenter.addCategory(category)
+
+        mutex.withLock {
+            bestAttemptContent.setCategoryIdentifier(category.identifier)
+        }
+
     }
 
     @OptIn(ExperimentalForeignApi::class)
-    private suspend fun createAttachments(userInfo: Map<String, Any>) = withContext(Dispatchers.Default) {
-        async {
-            val mediaUrlString = userInfo["image_url"] as? String ?: return@async
-            val mediaUrl = downloader.downloadFile(NSURL(string = mediaUrlString))
-            val attachment = UNNotificationAttachment.attachmentWithIdentifier(
-                mediaUrl!!.lastPathComponent!!,
-                mediaUrl,
-                null,
-                null
-            )
-            mutex.withLock {
-                bestAttemptContent.setAttachments(listOf(attachment))
-            }
+    private suspend fun createAttachments(userInfo: Map<String, Any>) {
+        val mediaUrlString = userInfo["image_url"] as? String ?: return
+        val mediaUrl = downloader.downloadFile(NSURL(string = mediaUrlString))
+        val attachment = UNNotificationAttachment.attachmentWithIdentifier(
+            mediaUrl!!.lastPathComponent!!,
+            mediaUrl,
+            null,
+            null
+        )
+        mutex.withLock {
+            bestAttemptContent.setAttachments(listOf(attachment))
         }
     }
 
-    private suspend fun createInApp(userInfo: Map<String, Any>) = withContext(Dispatchers.Default) {
-        async {
-            val ems = userInfo["ems"] as? Map<String, Any> ?: return@async
-            val inApp = ems["inapp"] as? Map<String, Any> ?: return@async
-            val inAppUrlString = inApp["url"] as? String ?: return@async
+    private suspend fun createInApp(userInfo: Map<String, Any>) {
+        val ems = userInfo["ems"] as? Map<String, Any> ?: return
+        val inApp = ems["inapp"] as? Map<String, Any> ?: return
+        val inAppUrlString = inApp["url"] as? String ?: return
 
-            val inAppData = downloader.downloadData(NSURL(string = inAppUrlString)) ?: return@async
+        val inAppData = downloader.downloadData(NSURL(string = inAppUrlString)) ?: return
 
-            val mutableInApp = inApp.toMutableMap()
-            val mutableEms = ems.toMutableMap()
-            val mutableUserInfo = userInfo.toMutableMap()
+        val mutableInApp = inApp.toMutableMap()
+        val mutableEms = ems.toMutableMap()
+        val mutableUserInfo = userInfo.toMutableMap()
 
-            mutableInApp["inAppData"] = inAppData
-            mutableEms["inapp"] = mutableInApp
-            mutableUserInfo["ems"] = mutableEms
+        mutableInApp["inAppData"] = inAppData
+        mutableEms["inapp"] = mutableInApp
+        mutableUserInfo["ems"] = mutableEms
 
-            mutex.withLock {
-                bestAttemptContent.setUserInfo(mutableUserInfo.toMap())
-            }
+        mutex.withLock {
+            bestAttemptContent.setUserInfo(mutableUserInfo.toMap())
         }
     }
-
 }
