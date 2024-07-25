7cc089eb2acb50ca6dd69d2f651107db0dc31a18
LasOri <LasOri@users.noreply.github.com>
2024-07-25 15:06:00 +0200

    test(device-client): Test case for anonym turned off SUITEDEV-36069
    Co-authored-by: matusekma <matusekma@users.noreply.github.com>
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>

--- emarsys-sdk/src/androidInstrumentedTest/kotlin/com/emarsys/integration/DeviceClientIntegrationTests.kt
+++ emarsys-sdk/src/androidInstrumentedTest/kotlin/com/emarsys/integration/DeviceClientIntegrationTests.kt
@@ -4,14 +4,16 @@ import com.emarsys.Emarsys
 import com.emarsys.EmarsysConfig
 import com.emarsys.di.DependencyContainerPrivateApi
 import com.emarsys.di.DependencyInjection
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import kotlinx.coroutines.test.runTest
+import kotlin.test.Ignore
 import kotlin.test.Test
 
 class DeviceClientIntegrationTests {
 
     @Test
-    fun testRegisterDeviceInfo() = runTest {
+    fun testRegisterDeviceInfo_whenAnonymContactIsTurnedOn() = runTest {
         val container = DependencyInjection.container as DependencyContainerPrivateApi
 
         Emarsys.initialize()
@@ -26,6 +28,25 @@ class DeviceClientIntegrationTests {
         container.sessionContext.contactToken shouldNotBe null
         container.sessionContext.refreshToken shouldNotBe null
         container.sessionContext.clientState shouldNotBe null
+    }
+
+    @Ignore //TODO: this is a real test case when the backend get rid off their inner flipper "SET_ANON_CONTACT_ON_CLIENT_REGISTRATION"
+    @Test
+    fun testRegisterDeviceInfo_whenAnonymContactIsTurnedOff() = runTest {
+        val container = DependencyInjection.container as DependencyContainerPrivateApi
+
+        Emarsys.initialize()
+        Emarsys.enableTracking(EmarsysConfig("EMSD1-A342A"))
+
+        container.sessionContext.contactToken = null
+        container.sessionContext.refreshToken = null
+        container.sessionContext.clientState = null
+
+        container.deviceClient.registerDeviceInfo()
+
+        container.sessionContext.contactToken shouldBe null
+        container.sessionContext.refreshToken shouldBe null
+        container.sessionContext.clientState shouldBe null
     }
 
 }
