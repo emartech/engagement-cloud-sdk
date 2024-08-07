187cc96ef9de8335af4b7d88684c911721df6900
megamegax <megamegax@gmail.com>
2024-08-07 13:56:51 +0200

    test(crypto): enable crypto tests SUITEDEV-36394
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: LordAndras <49073629+LordAndras@users.noreply.github.com>

--- emarsys-sdk/src/commonTest/kotlin/com/emarsys/core/crypto/CryptoTests.kt
+++ emarsys-sdk/src/commonTest/kotlin/com/emarsys/core/crypto/CryptoTests.kt
@@ -1,5 +1,12 @@
 package com.emarsys.core.crypto
 
+import com.emarsys.core.exceptions.DecryptionFailedException
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+
 class CryptoTests {
     private companion object {
         const val PUBLIC_KEY =
@@ -9,50 +16,50 @@ class CryptoTests {
 
     private val crypto = Crypto(PUBLIC_KEY)
 
-// TODO:   @Test
-//    fun testVerify_should_returnTrue() = runTest {
-//        val testMessage = "testData"
-//        val signature =
-//            "MEUCIQDb6AxUK2W4IyKJ/P02Y0BNlm2ioP7ytu3dOyumc4hN8gIgEzwKmeCtd6Jn9Neg4Epn+oSkV4wAJNmfAgeeAM0u7Nw="
-//
-//        val result = crypto.verify(testMessage, signature)
-//
-//        result shouldBe true
-//    }
-//
-//    @Test
-//    fun testVerify_should_returnFalse() = runTest {
-//        val testMessage = "testData2"
-//        val signature =
-//            "MEUCIQDb6AxUK2W4IyKJ/P02Y0BNlm2ioP7ytu3dOyumc4hN8gIgEzwKmeCtd6Jn9Neg4Epn+oSkV4wAJNmfAgeeAM0u7Nw="
-//
-//        val result = crypto.verify(testMessage, signature)
-//
-//        result shouldBe false
-//    }
-//
-//    @Test
-//    fun testEncryptAndDecrypt() = runTest {
-//        val testValue = "test message"
-//
-//        val encrypted = crypto.encrypt(testValue, TEST_SECRET)
-//
-//        encrypted shouldNotBe testValue
-//
-//        val decrypted = crypto.decrypt(encrypted, TEST_SECRET)
-//
-//        decrypted shouldBe testValue
-//    }
-//
-//    @Test
-//    fun testDecrypt_should_throw_anException_whenSecretIsWrong() = runTest {
-//        val testValue = "test message"
-//
-//        val encrypted = crypto.encrypt(testValue, TEST_SECRET)
-//
-//        encrypted shouldNotBe testValue
-//        shouldThrow<DecryptionFailedException> {
-//            crypto.decrypt(encrypted, TEST_SECRET + 2)
-//        }
-//    }
+    @Test
+    fun testVerify_should_returnTrue() = runTest {
+        val testMessage = "testData"
+        val signature =
+            "MEUCIQDb6AxUK2W4IyKJ/P02Y0BNlm2ioP7ytu3dOyumc4hN8gIgEzwKmeCtd6Jn9Neg4Epn+oSkV4wAJNmfAgeeAM0u7Nw="
+
+        val result = crypto.verify(testMessage, signature)
+
+        result shouldBe true
+    }
+
+    @Test
+    fun testVerify_should_returnFalse() = runTest {
+        val testMessage = "testData2"
+        val signature =
+            "MEUCIQDb6AxUK2W4IyKJ/P02Y0BNlm2ioP7ytu3dOyumc4hN8gIgEzwKmeCtd6Jn9Neg4Epn+oSkV4wAJNmfAgeeAM0u7Nw="
+
+        val result = crypto.verify(testMessage, signature)
+
+        result shouldBe false
+    }
+
+    @Test
+    fun testEncryptAndDecrypt() = runTest {
+        val testValue = "test message"
+
+        val encrypted = crypto.encrypt(testValue, TEST_SECRET)
+
+        encrypted shouldNotBe testValue
+
+        val decrypted = crypto.decrypt(encrypted, TEST_SECRET)
+
+        decrypted shouldBe testValue
+    }
+
+    @Test
+    fun testDecrypt_should_throw_anException_whenSecretIsWrong() = runTest {
+        val testValue = "test message"
+
+        val encrypted = crypto.encrypt(testValue, TEST_SECRET)
+
+        encrypted shouldNotBe testValue
+        shouldThrow<DecryptionFailedException> {
+            crypto.decrypt(encrypted, TEST_SECRET + 2)
+        }
+    }
 }
