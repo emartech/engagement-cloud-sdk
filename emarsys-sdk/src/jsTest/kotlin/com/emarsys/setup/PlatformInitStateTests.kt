a874bfd3c343c092f282b6608b5f99a204ab7466
matusekma <36794575+matusekma@users.noreply.github.com>
2024-08-08 09:58:15 +0200

    test(web-push): remove PushService from PlatformInitStateTests SUITEDEV-35853
    Co-authored-by: Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>

--- emarsys-sdk/src/jsTest/kotlin/com/emarsys/setup/PlatformInitStateTests.kt
+++ emarsys-sdk/src/jsTest/kotlin/com/emarsys/setup/PlatformInitStateTests.kt
@@ -1,11 +1,8 @@
 package com.emarsys.setup
 
-import com.emarsys.JsEmarsysConfig
 import com.emarsys.mobileengage.inapp.InappJsBridgeApi
-import com.emarsys.mobileengage.push.PushServiceApi
 import dev.mokkery.answering.returns
 import dev.mokkery.everySuspend
-import dev.mokkery.matcher.any
 import dev.mokkery.mock
 import dev.mokkery.verifySuspend
 import kotlinx.coroutines.test.runTest
@@ -13,32 +10,15 @@ import kotlin.test.BeforeTest
 import kotlin.test.Test
 
 class PlatformInitStateTests {
-    private companion object {
-        val testConfig = JsEmarsysConfig("testApplicationCode")
-    }
 
     private lateinit var platformInitState: PlatformInitState
-    private lateinit var mockPushService: PushServiceApi
     private lateinit var mockJsBridge: InappJsBridgeApi
 
     @BeforeTest
     fun setup() = runTest {
-        mockPushService = mock()
-        everySuspend { mockPushService.register(any()) } returns Unit
         mockJsBridge = mock()
         everySuspend { mockJsBridge.register() } returns Unit
         platformInitState = PlatformInitState(mockJsBridge)
-    }
-
-    @Test
-    fun activate_shouldNot_callRegister_onPushService_ifSdkContext_hasNoConfig() = runTest {
-        platformInitState.active()
-
-        verifySuspend {
-            repeat(0) {
-                mockPushService.register(testConfig)
-            }
-        }
     }
 
     @Test
