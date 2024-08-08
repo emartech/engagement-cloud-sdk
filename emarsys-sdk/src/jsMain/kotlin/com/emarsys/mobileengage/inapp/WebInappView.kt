ddc6238e38b83299fd68e833b688ce3e62076068
Andras Sarro <lordandras@users.noreply.github.com>
2024-08-08 15:45:15 +0200

    feat(inapp): implement inappView for web SUITEDEV-36204
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: matusekma <36794575+matusekma@users.noreply.github.com>

---
+++ emarsys-sdk/src/jsMain/kotlin/com/emarsys/mobileengage/inapp/WebInappView.kt
@@ -0,0 +1,29 @@
+package com.emarsys.mobileengage.inApp
+
+import web.dom.document
+import web.html.HTMLElement
+
+class WebInappView(private val inappScriptExtractor: InappScriptExtractorApi) : InAppViewApi {
+    var inappView: HTMLElement? = null
+
+    override suspend fun load(message: InAppMessage) {
+        val view = document.createElement("div")
+        view.innerHTML = message.content()
+
+        val scriptContents = inappScriptExtractor.extract(view)
+        createScriptElements(scriptContents).forEach { scriptElement ->
+            view.appendChild(scriptElement)
+        }
+
+        inappView = view
+    }
+
+    private fun createScriptElements(scriptTexts: List<String>): List<HTMLElement> {
+        return scriptTexts.map { scriptContent ->
+            val element = document.createElement("script")
+            element.setAttribute("type", "text/javascript")
+            element.textContent = scriptContent
+            element
+        }
+    }
+}
---
+++ emarsys-sdk/src/jsTest/kotlin/com/emarsys/mobileengage/inapp/WebInappViewTests.kt
@@ -0,0 +1,79 @@
+package com.emarsys.mobileengage.inApp
+
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import kotlinx.coroutines.test.runTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+class WebInappViewTests {
+
+    private lateinit var webInappView: WebInappView
+    private lateinit var inappScriptExtractor: InappScriptExtractorApi
+
+    @BeforeTest
+    fun setup() {
+        inappScriptExtractor = InappScriptExtractor()
+        webInappView = WebInappView(inappScriptExtractor)
+    }
+
+    @Test
+    fun load_shouldSetTheHtmlContent_andAddScripts() = runTest {
+        val testScriptContent1 = "script1"
+        val testScriptContent2 = "script2"
+        val testHtml = """<html><head>
+                <script>$testScriptContent1</script>
+                <script>$testScriptContent2</script>
+            </head>
+            <body class="en">
+            <div style="width: 100%">
+                    <h3>Inapp JS Bridge tests</h3>
+                    <button type="button" id="button2" me-button-clicked="buttonId">Button clicked!</button>
+                </div>
+            </body>
+            </html>"""
+
+        val testMessage = InAppMessage(url = testHtml)
+
+        webInappView.load(testMessage)
+
+        webInappView.inappView shouldNotBe null
+
+        webInappView.inappView?.let {
+            it.querySelectorAll("script").length shouldBe 4
+            it.innerHTML.contains("<script>$testScriptContent1</script>") shouldBe true
+            it.innerHTML.contains("<script>$testScriptContent2</script>") shouldBe true
+            it.querySelector("div") shouldNotBe null
+            it.querySelector("button") shouldNotBe null
+            it.querySelector("h3") shouldNotBe null
+        }
+    }
+
+    @Test
+    fun load_shouldSetTheHtmlContent_withOutScripts() = runTest {
+        val testHtml = """<html><head>
+                <title>Test title</title>
+            </head>
+            <body class="en">
+            <div style="width: 100%">
+                    <h3>Inapp JS Bridge tests</h3>
+                    <button type="button" id="button2" me-button-clicked="buttonId">Button clicked!</button>
+                </div>
+            </body>
+            </html>"""
+
+        val testMessage = InAppMessage(url = testHtml)
+
+        webInappView.load(testMessage)
+
+        webInappView.inappView shouldNotBe null
+
+        webInappView.inappView?.let {
+            it.querySelectorAll("script").length shouldBe 0
+            it.querySelector("div") shouldNotBe null
+            it.querySelector("button") shouldNotBe null
+            it.querySelector("h3") shouldNotBe null
+        }
+    }
+}
+
