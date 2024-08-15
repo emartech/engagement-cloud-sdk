c394090998347bd177681be0e05297b158497a03
matusekma <36794575+matusekma@users.noreply.github.com>
2024-08-15 15:27:16 +0200

    feat(inapp): create WebViewProvider SUITEDEV-36205
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: LordAndras <49073629+LordAndras@users.noreply.github.com>

---
+++ emarsys-sdk/src/androidInstrumentedTest/kotlin/com/emarsys/mobileengage/inapp/WebViewProviderTests.kt
@@ -0,0 +1,32 @@
+package com.emarsys.mobileengage.inapp
+
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import com.emarsys.applicationContext
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.junit.Test
+
+class WebViewProviderTests {
+
+    @Test
+    fun provider_shouldCreateWebView_withSettings() {
+        runBlocking {
+            val webViewProvider = WebViewProvider(applicationContext, Dispatchers.Main)
+
+            val webView = webViewProvider.provide()
+
+            withContext(Dispatchers.Main) {
+                with(webView) {
+                    settings.javaScriptEnabled shouldBe true
+                    settings.loadWithOverviewMode shouldBe true
+                    settings.useWideViewPort shouldBe true
+                    settings.domStorageEnabled shouldBe true
+                    layoutParams.height shouldBe MATCH_PARENT
+                    layoutParams.width shouldBe MATCH_PARENT
+                }
+            }
+        }
+    }
+}
