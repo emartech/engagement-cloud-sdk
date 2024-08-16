09b03f420d260d74c877ba7c42c878d2b5d4690f
davidSchuppa <davidSchuppa@users.noreply.github.com>
2024-08-16 14:29:16 +0200

    feat(storage): ios remove TODOS SUITEDEV-36434
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: Andras Sarro <lordandras@users.noreply.github.com>
    Co-authored-by: matusekma <36794575+matusekma@users.noreply.github.com>

--- emarsys-sdk/src/iosMain/kotlin/com/emarsys/core/storage/StringStorage.kt
+++ emarsys-sdk/src/iosMain/kotlin/com/emarsys/core/storage/StringStorage.kt
@@ -2,10 +2,9 @@ package com.emarsys.core.storage
 
 class StringStorage: TypedStorageApi<String?> {
     override fun put(key: String, value: String?) {
-        TODO("Not yet implemented")
     }
 
-    override fun get(key: String): String {
-        TODO("Not yet implemented")
+    override fun get(key: String): String? {
+        return null
     }
 }
