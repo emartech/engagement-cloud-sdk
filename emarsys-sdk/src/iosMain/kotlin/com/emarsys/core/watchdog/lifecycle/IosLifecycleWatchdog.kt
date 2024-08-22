ddcc9729d61b29a33359838b86e82e1f6884df8a
megamegax <megamegax@gmail.com>
2024-08-22 15:47:30 +0200

    feat(lifecycle-watchdog): add LifecycleWatchdog implementation for iOS SUITEDEV-36206
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

--- emarsys-sdk/src/iosMain/kotlin/com/emarsys/core/watchdog/lifecycle/IosLifecycleWatchdog.kt
+++ emarsys-sdk/src/iosMain/kotlin/com/emarsys/core/watchdog/lifecycle/IosLifecycleWatchdog.kt
@@ -2,14 +2,42 @@ package com.emarsys.core.watchdog.lifecycle
 
 import com.emarsys.core.actions.LifecycleEvent
 import com.emarsys.watchdog.lifecycle.LifecycleWatchDog
-import kotlinx.coroutines.flow.SharedFlow
 
-class IosLifecycleWatchdog: LifecycleWatchDog {
-    // mapping of lifecycles https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-lifecycle.html#ios
-    override val lifecycleEvents: SharedFlow<LifecycleEvent>
-        get() = TODO("Not yet implemented")
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import platform.Foundation.NSNotificationCenter
+import platform.UIKit.UIApplicationDidBecomeActiveNotification
+import platform.UIKit.UIApplicationDidEnterBackgroundNotification
+
+class IosLifecycleWatchdog : LifecycleWatchDog {
+
+    private val _lifecycleEvents = MutableSharedFlow<LifecycleEvent>()
+    override val lifecycleEvents: SharedFlow<LifecycleEvent> get() = _lifecycleEvents
 
     override suspend fun register() {
-        TODO("Not yet implemented")
+        NSNotificationCenter.defaultCenter.addObserverForName(
+            name = UIApplicationDidBecomeActiveNotification,
+            `object` = null,
+            queue = null
+        ) { _ ->
+            notifyLifecycleEvent(LifecycleEvent.OnForeground)
+        }
+
+        NSNotificationCenter.defaultCenter.addObserverForName(
+            name = UIApplicationDidEnterBackgroundNotification,
+            `object` = null,
+            queue = null
+        ) { _ ->
+            notifyLifecycleEvent(LifecycleEvent.OnBackground)
+        }
+    }
+
+    private fun notifyLifecycleEvent(event: LifecycleEvent) {
+        CoroutineScope(Dispatchers.Main).launch {
+            _lifecycleEvents.emit(event)
+        }
     }
 }
