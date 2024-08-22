a789bd4ed12daa07ba593933be85c30785ba62ac
megamegax <megamegax@gmail.com>
2024-08-22 15:17:12 +0200

    feat(device-info): add implementation to IosLanguageProvider SUITEDEV-36206
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

--- emarsys-sdk/src/iosMain/kotlin/com/emarsys/core/provider/IosLanguageProvider.kt
+++ emarsys-sdk/src/iosMain/kotlin/com/emarsys/core/provider/IosLanguageProvider.kt
@@ -1,9 +1,11 @@
 package com.emarsys.core.provider
 
 import com.emarsys.core.providers.Provider
+import platform.Foundation.NSLocale
+import platform.Foundation.preferredLanguages
 
 class IosLanguageProvider: Provider<String> {
     override fun provide(): String {
-        TODO("Not yet implemented")
+        return NSLocale.preferredLanguages()[0] as String
     }
 }
