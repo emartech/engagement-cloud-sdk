ad4bb42373b7ca6788e395bc294b72a5316750eb
davidSchuppa <davidSchuppa@users.noreply.github.com>
2024-10-17 14:39:59 +0200

    feat(action): created NotificationOpenedActionModel SUITEDEV-36719
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: Andras Sarro <lordandras@users.noreply.github.com>
    Co-authored-by: matusekma <36794575+matusekma@users.noreply.github.com>

--- emarsys-sdk/src/commonMain/kotlin/com/emarsys/mobileengage/action/models/ReportingActionModel.kt
+++ emarsys-sdk/src/commonMain/kotlin/com/emarsys/mobileengage/action/models/ReportingActionModel.kt
@@ -6,12 +6,10 @@ import kotlinx.serialization.Serializable
 interface ReportingActionModel
 
 interface PushButtonClickedActionModel: ReportingActionModel {
-    val id: String
     val sid: String
 }
 
 interface InAppButtonClickedActionModel: ReportingActionModel {
-    val id: String
     val campaignId: String
     val sid: String?
     val url: String?
@@ -20,15 +18,21 @@ interface InAppButtonClickedActionModel: ReportingActionModel {
 @Serializable
 @SerialName("pushButtonClicked")
 data class BasicPushButtonClickedActionModel(
-    override val id: String,
+    val id: String,
     override val sid: String
 ): BasicActionModel(), PushButtonClickedActionModel
 
 @Serializable
 @SerialName("inAppButtonClicked")
 data class BasicInAppButtonClickedActionModel(
-    override val id: String,
+    val id: String,
     override val campaignId: String,
     override val sid: String? = null,
     override val url: String? = null
 ): BasicActionModel(), InAppButtonClickedActionModel
+
+@Serializable
+@SerialName("notificationOpened")
+data class NotificationOpenedActionModel(
+    val sid: String? = null
+): BasicActionModel(), ReportingActionModel
