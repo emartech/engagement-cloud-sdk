d28873f899fa66c6cb3b82833f7e30edd48dec23
matusekma <36794575+matusekma@users.noreply.github.com>
2025-02-25 14:30:11 +0100

    feat(launch-application): use MODE_BACKGROUND_ACTIVITY_START_ALLOWED activity option to launch application on API35 and above epic: SDK-162 ticket: SDK-142
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>

--- emarsys-sdk/src/androidMain/kotlin/com/emarsys/core/launchapplication/LaunchApplicationHandler.kt
+++ emarsys-sdk/src/androidMain/kotlin/com/emarsys/core/launchapplication/LaunchApplicationHandler.kt
@@ -1,26 +1,48 @@
 package com.emarsys.core.launchapplication
 
+import android.app.ActivityOptions
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
 import com.emarsys.AndroidEmarsysConfig
 import com.emarsys.context.SdkContextApi
+import com.emarsys.core.device.AndroidVersionUtils.isUpsideDownCakeOrAbove
+import com.emarsys.core.log.Logger
 import com.emarsys.watchdog.activity.ActivityFinderApi
 
 class LaunchApplicationHandler(
     private val applicationContext: Context,
     private val activityFinder: ActivityFinderApi,
-    private val sdkContext: SdkContextApi
-): LaunchApplicationHandlerApi {
+    private val sdkContext: SdkContextApi,
+    private val sdkLogger: Logger
+) : LaunchApplicationHandlerApi {
 
     override suspend fun launchApplication() {
         if (activityFinder.currentActivity() == null) {
             val config = sdkContext.config as? AndroidEmarsysConfig
-            val intent = if (config?.launchActivityClass != null) {
+            val launchIntent = if (config?.launchActivityClass != null) {
                 Intent(applicationContext, config.launchActivityClass)
             } else {
                 applicationContext.packageManager.getLaunchIntentForPackage(applicationContext.packageName)
             }
-            applicationContext.startActivity(intent)
+
+            launchIntent?.let {
+                val activityOptions = if (isUpsideDownCakeOrAbove) {
+                    ActivityOptions.makeBasic()
+                        .setPendingIntentCreatorBackgroundActivityStartMode(ActivityOptions.MODE_BACKGROUND_ACTIVITY_START_ALLOWED)
+                        .toBundle()
+                } else null
+
+                PendingIntent.getActivity(
+                    applicationContext,
+                    0,
+                    launchIntent,
+                    FLAG_UPDATE_CURRENT or FLAG_IMMUTABLE,
+                    activityOptions
+                ).send()
+            } ?: sdkLogger.debug("LaunchApplicationHandler", "Could not create LaunchIntent")
         }
     }
 }
