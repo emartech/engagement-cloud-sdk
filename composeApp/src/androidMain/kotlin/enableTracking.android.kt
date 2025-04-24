279f093729819c2254495536de3c4f422afa1bfc
Andras Sarro <lordandras@users.noreply.github.com>
2025-04-24 09:31:18 +0200

    chore(android-app): change default applicationCode epic:SDK-162 ticket:SDK-313
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

--- composeApp/src/androidMain/kotlin/enableTracking.android.kt
+++ composeApp/src/androidMain/kotlin/enableTracking.android.kt
@@ -2,5 +2,5 @@ import com.emarsys.AndroidEmarsysConfig
 import com.emarsys.Emarsys
 
 actual suspend fun enableTracking() {
-    Emarsys.enableTracking(AndroidEmarsysConfig("EMSE3-B4341"))
+    Emarsys.enableTracking(AndroidEmarsysConfig("EMSA4-90136"))
 }
