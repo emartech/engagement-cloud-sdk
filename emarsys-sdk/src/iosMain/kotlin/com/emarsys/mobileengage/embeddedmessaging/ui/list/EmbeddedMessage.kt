e5417cfe9940d26e15203022277af802b9660b4c
megamegax <megamegax@gmail.com>
2025-11-12 11:29:37 +0100

    feat(embedded-messaging): move dependencies to EmbeddedMessagingInjection.kt SDK-558 SDK-581
    Co-authored-by: LordAndras <49073629+LordAndras@users.noreply.github.com>
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>

--- emarsys-sdk/src/iosMain/kotlin/com/emarsys/mobileengage/embeddedmessaging/ui/list/EmbeddedMessage.kt
+++ emarsys-sdk/src/iosMain/kotlin/com/emarsys/mobileengage/embeddedmessaging/ui/list/EmbeddedMessage.kt
@@ -1,16 +1,10 @@
 package com.emarsys.mobileengage.embeddedmessaging.ui.list
 
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.window.ComposeUIViewController
-import com.emarsys.core.util.DownloaderApi
-import com.emarsys.di.SdkKoinIsolationContext.koin
 import platform.UIKit.UIViewController
 
 fun embeddedMessage(): UIViewController {
     return ComposeUIViewController {
-        val imageDownloader = koin.get<DownloaderApi>()
-        val coroutineScope = rememberCoroutineScope()
-
-        ListPageView(ListPageViewModel(ListPageModel(), imageDownloader, coroutineScope = coroutineScope))
+        ListPageView()
     }
 }
