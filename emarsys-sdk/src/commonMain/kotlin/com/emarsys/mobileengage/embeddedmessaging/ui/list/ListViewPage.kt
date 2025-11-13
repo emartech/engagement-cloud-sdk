946b213975c685cdc3f558fa2006149a9e2d1265
megamegax <megamegax@gmail.com>
2025-11-13 16:11:47 +0100

    feat(embedded-messaging): add empty state to listPage SDK-558 SDK-585
    Co-authored-by: LordAndras <49073629+LordAndras@users.noreply.github.com>
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: Andras Farkas <224590688+FarkasAndrasDev@users.noreply.github.com>

--- emarsys-sdk/src/commonMain/kotlin/com/emarsys/mobileengage/embeddedmessaging/ui/list/ListViewPage.kt
+++ emarsys-sdk/src/commonMain/kotlin/com/emarsys/mobileengage/embeddedmessaging/ui/list/ListViewPage.kt
@@ -1,10 +1,15 @@
 package com.emarsys.mobileengage.embeddedmessaging.ui.list
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -13,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.emarsys.di.SdkKoinIsolationContext.koin
 import com.emarsys.mobileengage.embeddedmessaging.ui.item.MessageItemView
@@ -46,10 +52,31 @@ fun MessageList(viewModel: ListPageViewModelApi) {
         onRefresh = { viewModel.refreshMessages() },
         modifier = Modifier.fillMaxSize()
     ) {
-        LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(items = messages, key = { it.id }) { messageViewModel ->
-                MessageItemView(messageViewModel)
+       if (messages.isEmpty()) {
+            EmptyState()
+        } else {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(items = messages, key = { it.id }) { messageViewModel ->
+                    MessageItemView(messageViewModel)
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun EmptyState() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("No messages")
+            Text("You have no messages in the selected view.")
         }
     }
 }
