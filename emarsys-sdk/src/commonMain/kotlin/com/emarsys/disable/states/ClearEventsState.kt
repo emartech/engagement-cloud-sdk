0cf30af93f9f5a700ce8dbb319c67de1620805f2
Andras Sarro <lordandras@users.noreply.github.com>
2025-11-26 16:36:39 +0100

    feat(clear-events-state): add filtering for Operational and Log events to keep them in the DB epic:SDK-153 ticket:SDK-630
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: Andras Farkas <224590688+FarkasAndrasDev@users.noreply.github.com>

--- emarsys-sdk/src/commonMain/kotlin/com/emarsys/disable/states/ClearEventsState.kt
+++ emarsys-sdk/src/commonMain/kotlin/com/emarsys/disable/states/ClearEventsState.kt
@@ -2,7 +2,9 @@ package com.emarsys.disable.states
 
 import com.emarsys.core.db.events.EventsDaoApi
 import com.emarsys.core.state.State
+import com.emarsys.event.SdkEvent
 import com.emarsys.util.runCatchingWithoutCancellation
+import kotlinx.coroutines.flow.filter
 
 class ClearEventsState(private val eventsDao: EventsDaoApi) : State {
     override val name = "ClearEvents"
@@ -12,8 +14,12 @@ class ClearEventsState(private val eventsDao: EventsDaoApi) : State {
 
     override suspend fun active(): Result<Unit> {
         return runCatchingWithoutCancellation {
-            //TODO: should we keep log events?
-            eventsDao.removeAll()
+            eventsDao.getEvents()
+                .filter {
+                    it !is SdkEvent.Internal.LogEvent && it !is SdkEvent.Internal.OperationalEvent
+                }.collect { event ->
+                    eventsDao.removeEvent(event)
+                }
         }
     }
 
