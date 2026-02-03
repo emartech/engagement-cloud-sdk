8442cfc6e6dc6366083b309816f4e49bde3dfc91
davidSchuppa <davidSchuppa@users.noreply.github.com>
2026-02-03 16:51:16 +0100

    feat(embedded-messaging): match design on DeleteMessageItemConfirmationDialog, adjusted confirmation button, shapes and spacing epic: SDK-709 ticket: SDK-718
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- emarsys-sdk/src/commonComposeMain/kotlin/com/emarsys/mobileengage/embeddedmessaging/ui/item/DeleteMessageItemConfirmationDialog.kt
+++ emarsys-sdk/src/commonComposeMain/kotlin/com/emarsys/mobileengage/embeddedmessaging/ui/item/DeleteMessageItemConfirmationDialog.kt
@@ -1,34 +1,56 @@
 package com.emarsys.mobileengage.embeddedmessaging.ui.item
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.emarsys.mobileengage.embeddedmessaging.ui.EmbeddedMessagingUiConstants.Dimensions.LARGE_PADDING
 import com.emarsys.mobileengage.embeddedmessaging.ui.translation.LocalStringResources
 
 
 @Composable
-internal fun DeleteMessageItemConfirmationDialog(
+fun DeleteMessageItemConfirmationDialog(
     onDismiss: () -> Unit,
     onConfirm: () -> Unit,
 ) {
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        title = { Text(LocalStringResources.current.deleteMessageDialogTitle) },
-        text = { Text(LocalStringResources.current.deleteMessageDialogDescription) },
-        confirmButton = {
-            TextButton(
-                onClick = onConfirm
-            ) {
-                Text(LocalStringResources.current.deleteMessageDialogConfirmButtonLabel)
-            }
-        },
-        dismissButton = {
-            TextButton(
-                onClick = onDismiss
-            ) {
-                Text(LocalStringResources.current.deleteMessageDialogCancelButtonLabel)
-            }
-        }
-    )
+    MaterialTheme {
+        AlertDialog(
+            onDismissRequest = onDismiss,
+            title = { Text(LocalStringResources.current.deleteMessageDialogTitle) },
+            text = {
+                Text(
+                    text = LocalStringResources.current.deleteMessageDialogDescription,
+                    modifier = Modifier.padding(top = LARGE_PADDING, bottom = LARGE_PADDING)
+                )
+            },
+            confirmButton = {
+                Button(
+                    onClick = onConfirm,
+                    colors = ButtonColors(
+                        containerColor = MaterialTheme.colorScheme.primary,
+                        contentColor = MaterialTheme.colorScheme.onPrimary,
+                        disabledContainerColor = ButtonDefaults.buttonColors().disabledContainerColor,
+                        disabledContentColor = ButtonDefaults.buttonColors().disabledContentColor
+                    ),
+                    shape = MaterialTheme.shapes.large
+                ) {
+                    Text(LocalStringResources.current.deleteMessageDialogConfirmButtonLabel)
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = onDismiss
+                ) {
+                    Text(LocalStringResources.current.deleteMessageDialogCancelButtonLabel)
+                }
+            },
+            shape = MaterialTheme.shapes.large
+        )
+    }
 }
