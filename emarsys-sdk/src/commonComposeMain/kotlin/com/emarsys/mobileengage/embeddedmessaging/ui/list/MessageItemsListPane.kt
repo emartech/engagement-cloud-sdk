1834a802508542d84525f16a436e42c3d7a2ff5d
Andras Sarro <lordandras@users.noreply.github.com>
2026-02-03 13:54:39 +0100

    feat(list-view): add delete icon on hove to message list epic:SDK-709 ticket:SDK-732
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: Andras Farkas <224590688+FarkasAndrasDev@users.noreply.github.com>

--- emarsys-sdk/src/commonComposeMain/kotlin/com/emarsys/mobileengage/embeddedmessaging/ui/list/MessageItemsListPane.kt
+++ emarsys-sdk/src/commonComposeMain/kotlin/com/emarsys/mobileengage/embeddedmessaging/ui/list/MessageItemsListPane.kt
@@ -2,9 +2,13 @@ package com.emarsys.mobileengage.embeddedmessaging.ui.list
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.interaction.HoverInteraction
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -14,6 +18,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.Delete
@@ -22,6 +27,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
@@ -42,6 +48,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.paging.LoadState
@@ -50,6 +59,7 @@ import androidx.paging.compose.itemKey
 import com.emarsys.mobileengage.embeddedmessaging.ui.EmbeddedMessagingUiConstants.Dimensions.DEFAULT_PADDING
 import com.emarsys.mobileengage.embeddedmessaging.ui.EmbeddedMessagingUiConstants.Dimensions.FLOATING_ACTION_BUTTON_SIZE
 import com.emarsys.mobileengage.embeddedmessaging.ui.EmbeddedMessagingUiConstants.Dimensions.MESSAGE_ITEM_IMAGE_SIZE
+import com.emarsys.mobileengage.embeddedmessaging.ui.EmbeddedMessagingUiConstants.Dimensions.ZERO_PADDING
 import com.emarsys.mobileengage.embeddedmessaging.ui.item.CustomMessageItemViewModelApi
 import com.emarsys.mobileengage.embeddedmessaging.ui.item.DeleteMessageItemConfirmationDialog
 import com.emarsys.mobileengage.embeddedmessaging.ui.item.MessageItemView
@@ -211,12 +221,18 @@ fun MessageItemsListPane(
                                             )
                                         }
                                     } else {
-                                        MessageItemViewContainer(
-                                            messageViewModel,
-                                            selectedMessage,
-                                            onMessageClick,
-                                            customMessageItem
-                                        )
+                                        BoxWithDeleteIcon(
+                                            onDelete = {
+                                                messageIdToDelete = messageViewModel.id
+                                            }
+                                        ) {
+                                            MessageItemViewContainer(
+                                                messageViewModel,
+                                                selectedMessage,
+                                                onMessageClick,
+                                                customMessageItem
+                                            )
+                                        }
                                     }
                                 }
                             }
@@ -285,7 +301,70 @@ private fun RefreshableMessageItemsList(
 }
 
 @Composable
-fun MessageItemViewContainer(
+fun BoxWithDeleteIcon(
+    onDelete: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    var isRowHovered by remember { mutableStateOf(false) }
+
+    LaunchedEffect(null) {
+        interactionSource.interactions.collect { interaction ->
+            when (interaction) {
+                is HoverInteraction.Enter -> {
+                    isRowHovered = true
+                }
+
+                is HoverInteraction.Exit -> {
+                    isRowHovered = false
+                }
+            }
+        }
+    }
+    Box(
+        Modifier.fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .hoverable(interactionSource = interactionSource)
+    ) {
+        Row(
+            Modifier.fillMaxWidth()
+                .background(MaterialTheme.colorScheme.surface)
+        ) {
+            content()
+        }
+        if (isRowHovered) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .clip(CircleShape)
+                    .padding(ZERO_PADDING)
+                    .background(
+                        shape = CircleShape,
+                        brush = Brush.radialGradient(
+                            colorStops = arrayOf(
+                                0.0f to Color.Transparent,
+                                1f to MaterialTheme.colorScheme.surfaceVariant,
+                            )
+                        )
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                IconButton(
+                    modifier = Modifier.padding(ZERO_PADDING),
+                    onClick = { onDelete() }
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Delete,
+                        contentDescription = LocalStringResources.current.deleteIconButtonAltText
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun MessageItemViewContainer(
     messageViewModel: MessageItemViewModelApi,
     selectedMessage: MessageItemViewModelApi?,
     onMessageClick: (MessageItemViewModelApi) -> Unit,
