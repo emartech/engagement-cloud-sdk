976d1620f762d5420f673403be65a2b078f75c7a
matusekma <36794575+matusekma@users.noreply.github.com>
2026-02-11 16:05:28 +0100

    fix(date-formatting): set locale in js tests ticket: SDK-746 epic: SDK-709
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>

--- emarsys-sdk/src/jsTest/kotlin/com/emarsys/core/datetime/DateTimeFormatTest.kt
+++ emarsys-sdk/src/jsTest/kotlin/com/emarsys/core/datetime/DateTimeFormatTest.kt
@@ -1,6 +1,10 @@
 package com.emarsys.core.datetime
 
 import io.kotest.matchers.shouldBe
+import js.objects.Object
+import js.objects.TypedPropertyDescriptor
+import web.navigator.navigator
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.time.Instant
 
@@ -10,24 +14,34 @@ class DateTimeFormatTest {
         val TEST_TIMESTAMP = Instant.parse("2026-03-01T01:01:00Z").toEpochMilliseconds()
     }
 
+    @BeforeTest
+    fun setup() {
+        Object.defineProperty(
+            navigator, "language",
+            js("{}").unsafeCast<TypedPropertyDescriptor<String>>().apply {
+                get = { "en-US" }
+                configurable = true
+            })
+    }
+
     @Test
     fun `asLocaleFormattedHoursAndMinutes should format hours and minutes according to locale`() {
         val formattedTime = TEST_TIMESTAMP.asLocaleFormattedHoursAndMinutes()
 
-        formattedTime shouldBe "2:01" // TZ in karma config is set to Europe/Budapest
+        formattedTime shouldBe "2:01 AM" // TZ in karma config is set to Europe/Budapest
     }
 
     @Test
     fun `asLocaleFormattedMonthsAndDays should format hours and minutes according to locale`() {
         val formattedTime = TEST_TIMESTAMP.asLocaleFormattedMonthsAndDays()
 
-        formattedTime shouldBe "1 Mar"
+        formattedTime shouldBe "Mar 1"
     }
 
     @Test
     fun `asLocaleFormattedFullDate should format hours and minutes according to locale`() {
         val formattedTime = TEST_TIMESTAMP.asLocaleFormattedFullDate()
 
-        formattedTime shouldBe "01/03/2026"
+        formattedTime shouldBe "03/01/2026"
     }
 }
