cdc442b42ddc361771e3892c1dd6323ce49ae335
davidSchuppa <davidSchuppa@users.noreply.github.com>
2026-02-17 15:47:12 +0100

    fix(ios): fix old config usage epic:SDK-709 ticket:SDK-753
    Co-authored-by: Andras Sarro <lordandras@users.noreply.github.com>

--- composeApp/src/iosMain/kotlin/com/sap/ec/sample/enableTracking.ios.kt
+++ composeApp/src/iosMain/kotlin/com/sap/ec/sample/enableTracking.ios.kt
@@ -1,8 +1,8 @@
 package com.sap.ec.sample
 
-import com.sap.ec.IosEmarsysConfig
 import com.sap.ec.IosEngagementCloud
+import com.sap.ec.IosEngagementCloudSDKConfig
 
 actual suspend fun enableTracking() {
-    IosEngagementCloud.setup.enableTracking(IosEmarsysConfig("EMSE3-B4341"))
+    IosEngagementCloud.setup.enableTracking(IosEngagementCloudSDKConfig("EMSE3-B4341"))
 }
