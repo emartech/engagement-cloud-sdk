d537c51262fc2424f277d3e19c98889e98b90761
davidSchuppa <davidSchuppa@users.noreply.github.com>
2026-02-19 15:21:58 +0100

    feat(embedded-messaging): added color defaults for compose epic:SDK-709 ticket:SDK-718
    Co-authored-by: Andras Sarro <lordandras@users.noreply.github.com>

--- engagement-cloud-sdk/src/commonComposeMain/kotlin/com/sap/ec/mobileengage/embeddedmessaging/ui/EmbeddedMessagingUiConstants.kt
+++ engagement-cloud-sdk/src/commonComposeMain/kotlin/com/sap/ec/mobileengage/embeddedmessaging/ui/EmbeddedMessagingUiConstants.kt
@@ -1,6 +1,10 @@
 package com.sap.ec.mobileengage.embeddedmessaging.ui
 
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
+import com.sap.ec.mobileengage.embeddedmessaging.ui.theme.toColor
 
 object EmbeddedMessagingUiConstants {
 
@@ -27,4 +31,57 @@ object EmbeddedMessagingUiConstants {
         val ZERO_CORNER_RADIUS = 0.dp
     }
 
+    internal object Colors {
+        @Composable
+        fun getDefaultColorScheme(): ColorScheme = ColorScheme(
+            primary = "#526525".toColor(MaterialTheme.colorScheme.primary),
+            onPrimary = "#ffffff".toColor(MaterialTheme.colorScheme.onPrimary),
+            primaryContainer = "#9cbd4c".toColor(MaterialTheme.colorScheme.primaryContainer),
+            onPrimaryContainer = "#000000".toColor(MaterialTheme.colorScheme.onPrimaryContainer),
+            secondary = "#365e2c".toColor(MaterialTheme.colorScheme.secondary),
+            onSecondary = "#ffffff".toColor(MaterialTheme.colorScheme.onSecondary),
+            secondaryContainer = "#6ab158".toColor(MaterialTheme.colorScheme.secondaryContainer),
+            onSecondaryContainer = "#000000".toColor(MaterialTheme.colorScheme.onSecondaryContainer),
+            tertiary = "#6f6334".toColor(MaterialTheme.colorScheme.tertiary),
+            onTertiary = "#ffffff".toColor(MaterialTheme.colorScheme.onTertiary),
+            tertiaryContainer = "#b19f58".toColor(MaterialTheme.colorScheme.tertiaryContainer),
+            onTertiaryContainer = "#000000".toColor(MaterialTheme.colorScheme.onTertiaryContainer),
+            error = "#BA1A1A".toColor(MaterialTheme.colorScheme.error),
+            onError = "#FFFFFF".toColor(MaterialTheme.colorScheme.onError),
+            errorContainer = "#FFDAD6".toColor(MaterialTheme.colorScheme.errorContainer),
+            onErrorContainer = "#410002".toColor(MaterialTheme.colorScheme.onErrorContainer),
+            background = "#D4D4C0".toColor(MaterialTheme.colorScheme.background),
+            onBackground = "#1B1C18".toColor(MaterialTheme.colorScheme.onBackground),
+            surface = "#E9E9DD".toColor(MaterialTheme.colorScheme.surface),
+            onSurface = "#1B1C18".toColor(MaterialTheme.colorScheme.onSurface),
+            surfaceVariant = "#ffffff".toColor(MaterialTheme.colorScheme.surfaceVariant),
+            onSurfaceVariant = "#9dae75".toColor(MaterialTheme.colorScheme.onSurfaceVariant),
+            surfaceContainer = "#F0EFE8".toColor(MaterialTheme.colorScheme.surfaceContainer),
+            surfaceContainerHigh = "#EAE9E2".toColor(MaterialTheme.colorScheme.surfaceContainerHigh),
+            surfaceContainerHighest = "#E4E3DD".toColor(MaterialTheme.colorScheme.surfaceContainerHighest),
+            surfaceContainerLow = "#F6F5EE".toColor(MaterialTheme.colorScheme.surfaceContainerLow),
+            surfaceContainerLowest = "#FFFFFF".toColor(MaterialTheme.colorScheme.surfaceContainerLowest),
+            surfaceDim = "#DDD9D1".toColor(MaterialTheme.colorScheme.surfaceDim),
+            surfaceBright = "#FDFCF6".toColor(MaterialTheme.colorScheme.surfaceBright),
+            outline = "#c8cebb".toColor(MaterialTheme.colorScheme.outline),
+            outlineVariant = "#ffffff".toColor(MaterialTheme.colorScheme.outlineVariant),
+            inverseSurface = "#30312C".toColor(MaterialTheme.colorScheme.inverseSurface),
+            inverseOnSurface = "#F2F1E9".toColor(MaterialTheme.colorScheme.inverseOnSurface),
+            inversePrimary = "#f9fbf4".toColor(MaterialTheme.colorScheme.inversePrimary),
+            scrim = "#000000".toColor(MaterialTheme.colorScheme.scrim),
+            surfaceTint = "#526525".toColor(MaterialTheme.colorScheme.surfaceTint),
+            primaryFixed = "#ffffff".toColor(MaterialTheme.colorScheme.primaryFixed),
+            primaryFixedDim = "#e4edcf".toColor(MaterialTheme.colorScheme.primaryFixedDim),
+            onPrimaryFixed = "#000000".toColor(MaterialTheme.colorScheme.onPrimaryFixed),
+            onPrimaryFixedVariant = "#151a0a".toColor(MaterialTheme.colorScheme.onPrimaryFixedVariant),
+            secondaryFixed = "#ffffff".toColor(MaterialTheme.colorScheme.secondaryFixed),
+            secondaryFixedDim = "#d7ead2".toColor(MaterialTheme.colorScheme.secondaryFixedDim),
+            onSecondaryFixed = "#000000".toColor(MaterialTheme.colorScheme.onSecondaryFixed),
+            onSecondaryFixedVariant = "#0e190b".toColor(MaterialTheme.colorScheme.onSecondaryFixedVariant),
+            tertiaryFixed = "#ffffff".toColor(MaterialTheme.colorScheme.tertiaryFixed),
+            tertiaryFixedDim = "#eae5d2".toColor(MaterialTheme.colorScheme.tertiaryFixedDim),
+            onTertiaryFixed = "#000000".toColor(MaterialTheme.colorScheme.onTertiaryFixed),
+            onTertiaryFixedVariant = "#19160b".toColor(MaterialTheme.colorScheme.onTertiaryFixedVariant),
+        )
+    }
 }
--- engagement-cloud-sdk/src/commonComposeMain/kotlin/com/sap/ec/mobileengage/embeddedmessaging/ui/theme/ThemeMapper.kt
+++ engagement-cloud-sdk/src/commonComposeMain/kotlin/com/sap/ec/mobileengage/embeddedmessaging/ui/theme/ThemeMapper.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sap.ec.mobileengage.embeddedmessaging.EmbeddedMessagingContextApi
+import com.sap.ec.mobileengage.embeddedmessaging.ui.EmbeddedMessagingUiConstants
 import com.sap.ec.mobileengage.embeddedmessaging.ui.EmbeddedMessagingUiConstants.Dimensions.DEFAULT_ELEVATION
 import com.sap.ec.mobileengage.embeddedmessaging.ui.EmbeddedMessagingUiConstants.Dimensions.ZERO_ELEVATION
 import com.sap.ec.mobileengage.embeddedmessaging.ui.EmbeddedMessagingUiConstants.Shapes.ZERO_CORNER_RADIUS
@@ -22,56 +23,56 @@ internal class ThemeMapper(private val embeddedMessagingContext: EmbeddedMessagi
         val colors = embeddedMessagingContext.metaData?.design?.fillColor
         return colors?.let {
             ColorScheme(
-                primary = colors.primary.toColor(MaterialTheme.colorScheme.primary),
-                onPrimary = colors.onPrimary.toColor(MaterialTheme.colorScheme.onPrimary),
-                primaryContainer = colors.primaryContainer.toColor(MaterialTheme.colorScheme.primaryContainer),
-                onPrimaryContainer = colors.onPrimaryContainer.toColor(MaterialTheme.colorScheme.onPrimaryContainer),
-                secondary = colors.secondary.toColor(MaterialTheme.colorScheme.secondary),
-                onSecondary = colors.onSecondary.toColor(MaterialTheme.colorScheme.onSecondary),
-                secondaryContainer = colors.secondaryContainer.toColor(MaterialTheme.colorScheme.secondaryContainer),
-                onSecondaryContainer = colors.onSecondaryContainer.toColor(MaterialTheme.colorScheme.onSecondaryContainer),
-                tertiary = colors.tertiary.toColor(MaterialTheme.colorScheme.tertiary),
-                onTertiary = colors.onTertiary.toColor(MaterialTheme.colorScheme.onTertiary),
-                tertiaryContainer = colors.tertiaryContainer.toColor(MaterialTheme.colorScheme.tertiaryContainer),
-                onTertiaryContainer = colors.onTertiaryContainer.toColor(MaterialTheme.colorScheme.onTertiaryContainer),
-                error = colors.error.toColor(MaterialTheme.colorScheme.error),
-                onError = colors.onError.toColor(MaterialTheme.colorScheme.onError),
-                errorContainer = colors.errorContainer.toColor(MaterialTheme.colorScheme.errorContainer),
-                onErrorContainer = colors.onErrorContainer.toColor(MaterialTheme.colorScheme.onErrorContainer),
-                background = colors.background.toColor(MaterialTheme.colorScheme.background),
-                onBackground = colors.onBackground.toColor(MaterialTheme.colorScheme.onBackground),
-                surface = colors.surface.toColor(MaterialTheme.colorScheme.surface),
-                onSurface = colors.onSurface.toColor(MaterialTheme.colorScheme.onSurface),
-                surfaceVariant = colors.surfaceVariant.toColor(MaterialTheme.colorScheme.surfaceVariant),
-                onSurfaceVariant = colors.onSurfaceVariant.toColor(MaterialTheme.colorScheme.onSurfaceVariant),
-                surfaceContainer = colors.surfaceContainer.toColor(MaterialTheme.colorScheme.surfaceContainer),
-                surfaceContainerHigh = colors.surfaceContainerHigh.toColor(MaterialTheme.colorScheme.surfaceContainerHigh),
-                surfaceContainerHighest = colors.surfaceContainerHighest.toColor(MaterialTheme.colorScheme.surfaceContainerHighest),
-                surfaceContainerLow = colors.surfaceContainerLow.toColor(MaterialTheme.colorScheme.surfaceContainerLow),
-                surfaceContainerLowest = colors.surfaceContainerLowest.toColor(MaterialTheme.colorScheme.surfaceContainerLowest),
-                surfaceDim = colors.surfaceDim.toColor(MaterialTheme.colorScheme.surfaceDim),
-                surfaceBright = colors.surfaceBright.toColor(MaterialTheme.colorScheme.surfaceBright),
-                outline = colors.outline.toColor(MaterialTheme.colorScheme.outline),
-                outlineVariant = colors.outlineVariant.toColor(MaterialTheme.colorScheme.outlineVariant),
-                inverseSurface = colors.inverseSurface.toColor(MaterialTheme.colorScheme.inverseSurface),
-                inverseOnSurface = colors.inverseOnSurface.toColor(MaterialTheme.colorScheme.inverseOnSurface),
-                inversePrimary = colors.inversePrimary.toColor(MaterialTheme.colorScheme.inversePrimary),
-                scrim = colors.scrim.toColor(MaterialTheme.colorScheme.scrim),
-                surfaceTint = colors.surfaceTint.toColor(MaterialTheme.colorScheme.surfaceTint),
-                primaryFixed = colors.primaryFixed.toColor(MaterialTheme.colorScheme.primaryFixed),
-                primaryFixedDim = colors.primaryFixedDim.toColor(MaterialTheme.colorScheme.primaryFixedDim),
-                onPrimaryFixed = colors.onPrimaryFixed.toColor(MaterialTheme.colorScheme.onPrimaryFixed),
-                onPrimaryFixedVariant = colors.onPrimaryFixedVariant.toColor(MaterialTheme.colorScheme.onPrimaryFixedVariant),
-                secondaryFixed = colors.secondaryFixed.toColor(MaterialTheme.colorScheme.secondaryFixed),
-                secondaryFixedDim = colors.secondaryFixedDim.toColor(MaterialTheme.colorScheme.secondaryFixedDim),
-                onSecondaryFixed = colors.onSecondaryFixed.toColor(MaterialTheme.colorScheme.onSecondaryFixed),
-                onSecondaryFixedVariant = colors.onSecondaryFixedVariant.toColor(MaterialTheme.colorScheme.onSecondaryFixedVariant),
-                tertiaryFixed = colors.tertiaryFixed.toColor(MaterialTheme.colorScheme.tertiaryFixed),
-                tertiaryFixedDim = colors.tertiaryFixedDim.toColor(MaterialTheme.colorScheme.tertiaryFixedDim),
-                onTertiaryFixed = colors.onTertiaryFixed.toColor(MaterialTheme.colorScheme.onTertiaryFixed),
-                onTertiaryFixedVariant = colors.onTertiaryFixedVariant.toColor(MaterialTheme.colorScheme.onTertiaryFixedVariant)
+                primary = colors.primary.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().primary),
+                onPrimary = colors.onPrimary.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onPrimary),
+                primaryContainer = colors.primaryContainer.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().primaryContainer),
+                onPrimaryContainer = colors.onPrimaryContainer.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onPrimaryContainer),
+                secondary = colors.secondary.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().secondary),
+                onSecondary = colors.onSecondary.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onSecondary),
+                secondaryContainer = colors.secondaryContainer.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().secondaryContainer),
+                onSecondaryContainer = colors.onSecondaryContainer.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onSecondaryContainer),
+                tertiary = colors.tertiary.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().tertiary),
+                onTertiary = colors.onTertiary.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onTertiary),
+                tertiaryContainer = colors.tertiaryContainer.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().tertiaryContainer),
+                onTertiaryContainer = colors.onTertiaryContainer.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onTertiaryContainer),
+                error = colors.error.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().error),
+                onError = colors.onError.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onError),
+                errorContainer = colors.errorContainer.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().errorContainer),
+                onErrorContainer = colors.onErrorContainer.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onErrorContainer),
+                background = colors.background.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().background),
+                onBackground = colors.onBackground.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onBackground),
+                surface = colors.surface.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().surface),
+                onSurface = colors.onSurface.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onSurface),
+                surfaceVariant = colors.surfaceVariant.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().surfaceVariant),
+                onSurfaceVariant = colors.onSurfaceVariant.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onSurfaceVariant),
+                surfaceContainer = colors.surfaceContainer.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().surfaceContainer),
+                surfaceContainerHigh = colors.surfaceContainerHigh.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().surfaceContainerHigh),
+                surfaceContainerHighest = colors.surfaceContainerHighest.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().surfaceContainerHighest),
+                surfaceContainerLow = colors.surfaceContainerLow.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().surfaceContainerLow),
+                surfaceContainerLowest = colors.surfaceContainerLowest.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().surfaceContainerLowest),
+                surfaceDim = colors.surfaceDim.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().surfaceDim),
+                surfaceBright = colors.surfaceBright.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().surfaceBright),
+                outline = colors.outline.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().outline),
+                outlineVariant = colors.outlineVariant.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().outlineVariant),
+                inverseSurface = colors.inverseSurface.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().inverseSurface),
+                inverseOnSurface = colors.inverseOnSurface.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().inverseOnSurface),
+                inversePrimary = colors.inversePrimary.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().inversePrimary),
+                scrim = colors.scrim.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().scrim),
+                surfaceTint = colors.surfaceTint.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().surfaceTint),
+                primaryFixed = colors.primaryFixed.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().primaryFixed),
+                primaryFixedDim = colors.primaryFixedDim.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().primaryFixedDim),
+                onPrimaryFixed = colors.onPrimaryFixed.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onPrimaryFixed),
+                onPrimaryFixedVariant = colors.onPrimaryFixedVariant.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onPrimaryFixedVariant),
+                secondaryFixed = colors.secondaryFixed.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().secondaryFixed),
+                secondaryFixedDim = colors.secondaryFixedDim.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().secondaryFixedDim),
+                onSecondaryFixed = colors.onSecondaryFixed.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onSecondaryFixed),
+                onSecondaryFixedVariant = colors.onSecondaryFixedVariant.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onSecondaryFixedVariant),
+                tertiaryFixed = colors.tertiaryFixed.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().tertiaryFixed),
+                tertiaryFixedDim = colors.tertiaryFixedDim.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().tertiaryFixedDim),
+                onTertiaryFixed = colors.onTertiaryFixed.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onTertiaryFixed),
+                onTertiaryFixedVariant = colors.onTertiaryFixedVariant.toColor(EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme().onTertiaryFixedVariant)
             )
-        } ?: MaterialTheme.colorScheme
+        } ?: EmbeddedMessagingUiConstants.Colors.getDefaultColorScheme()
     }
 
     @Composable
@@ -137,7 +138,7 @@ internal class ThemeMapper(private val embeddedMessagingContext: EmbeddedMessagi
         } ?: MaterialTheme.shapes
     }
 
-    fun mapMisc() : EmbeddedMessagingDesignValues {
+    fun mapMisc(): EmbeddedMessagingDesignValues {
         val miscMetaData = embeddedMessagingContext.metaData?.design?.misc
         return miscMetaData?.let {
             EmbeddedMessagingDesignValues(
@@ -161,20 +162,21 @@ internal class ThemeMapper(private val embeddedMessagingContext: EmbeddedMessagi
         } ?: EmbeddedMessagingDesignValues()
     }
 
-    private fun String.toColor(fallback: Color): Color {
-        return try {
-            val colorString = this.removePrefix("#")
-            when (colorString.length) {
-                6 -> Color(("FF$colorString").toLong(16))
-                8 -> Color(colorString.toLong(16))
-                else -> fallback
-            }
-        } catch (_: Exception) {
-            fallback
-        }
-    }
 
     private fun Double.toTextStyle(): TextStyle {
         return TextStyle(fontSize = this.sp)
+    }
+}
+
+internal fun String.toColor(fallback: Color): Color {
+    return try {
+        val colorString = this.removePrefix("#")
+        when (colorString.length) {
+            6 -> Color(("FF$colorString").toLong(16))
+            8 -> Color(colorString.toLong(16))
+            else -> fallback
+        }
+    } catch (_: Exception) {
+        fallback
     }
 }
