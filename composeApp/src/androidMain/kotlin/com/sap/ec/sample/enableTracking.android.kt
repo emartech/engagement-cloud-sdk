646c2caf3f7821ef989f49b9f71ead51b5c684f6
matusekma <36794575+matusekma@users.noreply.github.com>
2026-03-02 16:53:13 +0100

    feat(events): remove event subscription from composeApp Android enableTracking ticket: SDK-754 epic: SDK-709

--- composeApp/src/androidMain/kotlin/com/sap/ec/sample/enableTracking.android.kt
+++ composeApp/src/androidMain/kotlin/com/sap/ec/sample/enableTracking.android.kt
@@ -1,10 +1,8 @@
 package com.sap.ec.sample
 
-import android.util.Log
 import com.sap.ec.AndroidEngagementCloud
 import com.sap.ec.api.config.AndroidEngagementCloudSDKConfig
 import com.sap.ec.config.LinkContactData
-import com.sap.ec.event.SdkEvent
 
 actual suspend fun enableTracking() {
     AndroidEngagementCloud.setup.enable(
@@ -13,8 +11,4 @@ actual suspend fun enableTracking() {
             LinkContactData.ContactFieldValueData("test1@test.com")
         }
     )
-    AndroidEngagementCloud.events.collect {
-        val appEvent = it as SdkEvent.External.Api.AppEvent
-        Log.d("AppEvent", "AppEvent: ${appEvent.name}}")
-    }
 }
