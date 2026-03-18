98df472eb61c0a509548cdebedc6cd4ee519049b
megamegax <megamegax@users.noreply.github.com>
2026-03-18 15:43:52 +0000

    deploy: 88c500d291a0dbf763bc00dadd0f1a14820e3763

--- snapshots/engagement-cloud-sdk-loader.js
+++ snapshots/engagement-cloud-sdk-loader.js
@@ -8,7 +8,7 @@
 
   const currentSource = document.currentScript.src;
   const baseUrl = currentSource.substring(0, currentSource.lastIndexOf("/"));
-  const sdkUrl = "https://emartech.github.io/engagement-cloud-sdk/0.0.247-45/engagement-cloud-sdk.js";
+  const sdkUrl = "https://emartech.github.io/engagement-cloud-sdk/0.0.247-46/engagement-cloud-sdk.js";
 //  const sdkUrl = `${baseUrl}/engagement-cloud-sdk-html.js`;
 
   global.EngagementCloud = {
