ef3095ff6a9ef819cbadb82936660456fc1488eb
davidSchuppa <davidSchuppa@users.noreply.github.com>
2026-03-24 16:13:19 +0100

    refactor(message-detail-view): messageDetailView accepts url and trackingInfo instead of viewModel epic: SDK-739 ticket: SDK-944
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- engagement-cloud-sdk/src/commonComposeMain/kotlin/com/sap/ec/mobileengage/embeddedmessaging/ui/detail/MessageDetailView.kt
+++ engagement-cloud-sdk/src/commonComposeMain/kotlin/com/sap/ec/mobileengage/embeddedmessaging/ui/detail/MessageDetailView.kt
@@ -3,33 +3,25 @@ package com.sap.ec.mobileengage.embeddedmessaging.ui.detail
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.sap.ec.mobileengage.embeddedmessaging.ui.item.MessageItemViewModelApi
 import com.sap.ec.mobileengage.embeddedmessaging.ui.theme.EmbeddedMessagingTheme
 import com.sap.ec.mobileengage.inapp.view.InlineInAppView
+import io.ktor.http.Url
 
 @Composable
 internal fun MessageDetailView(
-    messageViewModel: MessageItemViewModelApi,
-    onBack: () -> Unit,
-    modifier: Modifier
+    richContentUrl: Url,
+    trackingInfo: String,
+    modifier: Modifier,
+    onBack: () -> Unit
 ) {
     EmbeddedMessagingTheme {
         Column(modifier = modifier.fillMaxSize()) {
-            val url = remember {
-                messageViewModel.richContentUrl
-            }
-
-            if (url != null) {
-                InlineInAppView(
-                    url = url,
-                    messageViewModel.trackingInfo,
-                    onBack
-                )
-            } else {
-                //TODO: error handling
-            }
+            InlineInAppView(
+                url = richContentUrl,
+                trackingInfo,
+                onBack
+            )
         }
     }
 }
